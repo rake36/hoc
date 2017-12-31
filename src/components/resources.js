c7c191288de18de1ddc63832738585ca580bd178
Matt Rakestraw <rake36@gmail.com>
2017-12-31 16:05:58 -0500

    add logic for sign-in/sign-out

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Candied Pecans
+            <ul>
+                <li>1 egg white</li>
+                <li>1 tsp water</li>
+                <li>3/4 cup sugar</li>
+                <li>1 tbsp cinnamon</li>
+                <li>1 lb Pecans</li>
+            </ul>
+        </div>
+    );
+}
